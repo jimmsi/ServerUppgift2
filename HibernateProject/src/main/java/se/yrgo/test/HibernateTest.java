df6ac46473583061584efd7d26de45e951e57bd9
jimmsi <jimmyem@gmail.com>
2024-05-29 13:37:01 +0200

    completed assignments

--- HibernateProject/src/main/java/se/yrgo/test/HibernateTest.java
+++ HibernateProject/src/main/java/se/yrgo/test/HibernateTest.java
@@ -51,17 +51,77 @@ public class HibernateTest
         }
         */
 
+        /*
         Query q2 =em.createQuery("select tutor.teachingGroup from Tutor as tutor where tutor.name= 'Johan Smith'");
         List<Student> studentsForJohan = q2.getResultList();
         for (Student s : studentsForJohan) {
             System.out.println(s);
         }
 
+        */
+
+        /*
         Subject programming = em.find(Subject.class, 3);
         TypedQuery<Tutor> query= em.createQuery("from Tutor tutor where :subject member of tutor.subjectsToTeach",Tutor.class);
         query.setParameter("subject", programming);
         List<Tutor>tutorsForProgramming = query.getResultList();
         for(Tutor tutor : tutorsForProgramming) {
+            System.out.println(tutor);
+        }
+        */
+
+        // UPPGIFT 1
+
+        System.out.printf("Svar på uppgift 1:\n\n");
+
+        Subject science = em.find(Subject.class, 2);
+
+        Query query = em.createQuery("select student.name from Tutor tutor join tutor.teachingGroup student where :subject member of tutor.subjectsToTeach");
+        query.setParameter("subject", science);
+        List<String> studentNames = query.getResultList();
+        for (String name : studentNames) {
+            System.out.println(name);
+        }
+
+        System.out.println();
+
+        // UPPGIFT 2
+
+        System.out.printf("Svar på uppgift 2:\n\n");
+
+        List<Object[]>results = em.createQuery("select s.name, t.name from Tutor t join t.teachingGroup s").getResultList();
+
+        for(Object[] obj:results) {
+            System.out.println("Student name: " + obj[0]);
+            System.out.println("Tutor name " + obj[1]);
+        }
+
+        System.out.println();
+
+        // UPPGIFT 3
+
+        System.out.printf("Svar på uppgift 3:\n\n");
+
+        Double averageNrOfSemesters = (Double) em.createQuery("select avg(subject.numberOfSemesters) from Subject subject").getSingleResult();
+        System.out.println("Average number of semesters: " + averageNrOfSemesters);
+
+        System.out.println();
+
+        // UPPGIFT 4
+
+        System.out.printf("Svar på uppgift 4:\n\n");
+
+        int maxSalary = (int) em.createQuery("select max(tutor.salary) from Tutor tutor").getSingleResult();
+        System.out.println("Max salary: " + maxSalary);
+
+        System.out.println();
+
+        // UPPGIFT 5
+
+        System.out.printf("Svar på uppgift 5:\n\n");
+
+        List<Tutor> highSalaryTutors = em.createNamedQuery("showAllWithHighSalary", Tutor.class).getResultList();
+        for (Tutor tutor : highSalaryTutors) {
             System.out.println(tutor);
         }
 
